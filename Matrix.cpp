7af30810edf7c3addc25b9f5cba7ad5d748f90db
gilivex <gilivex@gmail.com>
2022-04-03 15:45:56 +0300

    my first commit

---
+++ Matrix.cpp
@@ -0,0 +1,7 @@
+//
+// Created by gilivex on 3.4.2022.
+//
+
+#include "Matrix.hpp"
+
+
---
+++ Matrix.hpp
@@ -0,0 +1,14 @@
+//
+// Created by gilivex on 3.4.2022.
+//
+
+#ifndef MATRIX_CALCULATOR_A_MATRIX_H
+#define MATRIX_CALCULATOR_A_MATRIX_H
+
+
+class matrix {
+
+};
+
+
+#endif //MATRIX_CALCULATOR_A_MATRIX_H
